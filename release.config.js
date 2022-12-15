ebdbc21dd96b7d9342d7f01c811b385e1bafb315
David <david@allido.io>
2022-12-15 09:59:22 -0800

    fix(deps): move to dmeents/semantic-release-yarn

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@suin/semantic-release-yarn', { npmPublish: true }],
+    ['@dmeents/semantic-release-yarn', { npmPublish: true }],
     [
       '@semantic-release/git',
       {
